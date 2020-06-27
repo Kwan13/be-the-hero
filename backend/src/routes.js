d4d450c17a36ed58a258ef16cbf9f3ec0d88b4d3
Kwan13 <kawan-silva@outlook.com>
2020-06-27 14:25:39 -0300

    add route create ong "/ ong"

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.send('Hello World!');
+routes.post('/ongs', async (request, response) => {
+    const { name, email, whatsapp, city, uf } = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    });
+
+    return response.json({ id });
 });
 
 module.exports = routes;
