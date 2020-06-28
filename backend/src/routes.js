001230ddc0d0fca00ea2f90a4530f77b73d077f1
Kwan13 <kawan-silva@outlook.com>
2020-06-28 14:01:19 -0300

    route delete incidents "/incidents/id"

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,5 +7,6 @@ routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
