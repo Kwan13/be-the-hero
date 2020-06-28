609b8cd86f9077b58841239755c33e37ba006ef9
Kwan13 <kawan-silva@outlook.com>
2020-06-28 14:40:17 -0300

    add route "/profile"

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
+const ProfileController = require('./controllers/ProfileController');
 const IncidentController = require('./controllers/IncidentController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+routes.get('/profile', ProfileController.index);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
