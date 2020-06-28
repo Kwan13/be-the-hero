be07f1b8a3b8b76c9b3ae501a37233aea44165e4
Kwan13 <kawan-silva@outlook.com>
2020-06-28 13:11:28 -0300

    add route create incident "/incidents"

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
