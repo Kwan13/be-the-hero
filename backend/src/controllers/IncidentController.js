5418fa461fc12b66f9c9eedba0631b9bc53885c8
Kwan13 <kawan-silva@outlook.com>
2020-06-29 16:32:50 -0300

    refactoring code

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,15 +2,21 @@ const connection = require("../database/connection");
 
 module.exports = {
     async index(request, response) {
-
         const { page = 1 } = request.query;
-
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
             .offset((page - 1) * 5)
-            .select('*');
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ]);
 
         response.header('X-total-count', count['count(*)']);
         return response.json(incidents);
@@ -44,7 +50,6 @@ module.exports = {
         }
 
         await connection('incidents').where('id', id).delete();
-
         return response.status(204).send();
     }
 }
