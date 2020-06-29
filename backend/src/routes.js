6a1fcaef673ded3346a8145b02950fab46f1fe0e
Kwan13 <kawan-silva@outlook.com>
2020-06-29 14:49:31 -0300

    add route sessions "/sessions"

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const OngController = require('./controllers/OngController');
 const ProfileController = require('./controllers/ProfileController');
 const IncidentController = require('./controllers/IncidentController');
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 routes.get('/profile', ProfileController.index);
 routes.get('/incidents', IncidentController.index);
+routes.post('/sessions', SessionController.create);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
